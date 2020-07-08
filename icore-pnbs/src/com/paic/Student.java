7f6514603ee74d860a4fbf69bd7a1688af3b697e
yshen <346692913@qq.com>
2020-07-09 00:56:24 +0800

    commit student

---
+++ icore-pnbs/src/com/paic/Student.java
@@ -0,0 +1,22 @@
+package com.paic;
+
+public class Student {
+    private int age;//年龄
+    private String id;//id
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
